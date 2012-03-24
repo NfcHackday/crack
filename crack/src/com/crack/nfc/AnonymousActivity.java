2666a5e0808ee55b0cfd598527b547d680b454b8
Nathan Sudell <nathan@corp.badoo.com>
2012-03-24 16:17:52 +0000

    Add Profile activity

---
+++ crack/src/com/crack/nfc/AnonymousActivity.java
@@ -0,0 +1,34 @@
+package com.crack.nfc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnonymousActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    	setContentView(R.layout.anon);
+    	
+        Button buttonFacebookSignin = (Button) findViewById(R.id.facebookSignin);
+        
+        buttonFacebookSignin.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Toast.makeText(getApplicationContext(), "Sign in", Toast.LENGTH_SHORT).show();
+				// Orene's code here....
+				
+	        	Intent profileScreen = new Intent(AnonymousActivity.this,ProfileActivity.class);
+	        	startActivity(profileScreen);
+			}
+		});
+        
+    }	
+	
+}
--- crack/src/com/crack/nfc/CrackActivity.java
+++ crack/src/com/crack/nfc/CrackActivity.java
@@ -1,29 +1,28 @@
 package com.crack.nfc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class CrackActivity extends Activity {
+	
+	private boolean authenticated = false;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.main);
-        
-        
-        Button buttonFacebookSignin = (Button) findViewById(R.id.facebookSignin);
-        
-        buttonFacebookSignin.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View arg0) {
-				Toast.makeText(getApplicationContext(), "Sign in", Toast.LENGTH_SHORT).show();
-				// Orene's code here....
-			}
-		});
+        if (this.authenticated) {
+        	Intent profileScreen = new Intent(this,ProfileActivity.class);
+        	startActivity(profileScreen);
+        } else {
+        	Intent anonScreen = new Intent(this,AnonymousActivity.class);
+        	startActivity(anonScreen);
+        }
         
     }
 }
