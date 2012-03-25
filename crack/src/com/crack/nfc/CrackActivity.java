442bca56440eb3a94a42d93af3cc8a7777b31df1
oren <neerdavid@hotmail.com>
2012-03-25 13:54:35 +0100

    getting the personObject

--- crack/src/com/crack/nfc/CrackActivity.java
+++ crack/src/com/crack/nfc/CrackActivity.java
@@ -1,10 +1,19 @@
 package com.crack.nfc;
 
+import java.io.BufferedInputStream;
+import java.io.FilterInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.http.AndroidHttpClient;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -21,6 +30,7 @@ import android.widget.Toast;
 
 import com.crack.storage.Friend;
 import com.crack.storage.Repository;
+import com.facebook.android.Utility;
 
 public class CrackActivity extends Activity implements CreateNdefMessageCallback, OnNdefPushCompleteCallback {
 	private final static String textLump = "Twas brillig, and the slithy toves Did gyre and gimble in the wabe: All mimsy were the borogoves, And the mome raths outgrabe.  Beware the Jabberwock, my son!  The jaws that bite, the claws that catch!  Beware the Jubjub bird, and shun The frumious Bandersnatch! He took his vorpal sword in hand: Long time the manxome foe he sought -- So rested he by the Tumtum tree, And stood awhile in thought.  And, as in uffish thought he stood, The Jabberwock, with eyes of flame, Came whiffling through the tulgey wood, And burbled as it came!  One, two! One, two! And through and through The vorpal blade went snicker-snack!  He left it dead, and with its head He went galumphing back.  And, has thou slain the Jabberwock?  Come to my arms, my beamish boy!  O frabjous day! Callooh! Callay!' He chortled in his joy.  `Twas brillig, and the slithy toves Did gyre and gimble in the wabe; All mimsy were the borogoves, And the mome raths outgrabe.";
@@ -51,6 +61,7 @@ public class CrackActivity extends Activity implements CreateNdefMessageCallback
 			}
 			
 		});
+        
         // END temp code
         
         //mInfoText = (TextView) findViewById(R.id.textView);
@@ -188,5 +199,4 @@ public class CrackActivity extends Activity implements CreateNdefMessageCallback
 		}
 		return null;
 	}
-
 }
